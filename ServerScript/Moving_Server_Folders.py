70176c1f34fc8997089f2d4d7d34828468c7277a
stefanou <stefanou@mit.edu>
2018-04-24 02:59:02 -0400

    Hopefully the Last Change for the Night

--- ServerScript/Moving_Server_Folders.py
+++ ServerScript/Moving_Server_Folders.py
@@ -1,18 +1,20 @@
 import shutil
 import os
 
-def moving_server_files(filename, directory): 
+def moving_server_files(filename, server, directory): 
 	'''
 	This function moves files from the where the go get command 
 	places the retrieved/downloaded files 
 	and places them in the correct created directory. 
 	Inputs: filename - string, the name of the file that has to be moved; directory - string, 
-	the name of the directory that it has to be moved to
+	the name of the directory that it has to be moved to; server - the server of interest
 	Outputs: None --> returns nothing
 	'''
 	source = os.environ['GOPATH']
 	
 	desination = os.getcwd() #get the current directory path, because it has the final location of the file
+
+	path = os.join(source, 'github.com/SendFriend/stellar-backend/' + )
 
 	files = os.listdir(source) #represent all the files in the directory as a list
 
